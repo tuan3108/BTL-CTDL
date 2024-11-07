73c5ee232c6ef064b6cd562520a4c4f6401c4e28
tuan3108 <166523553+tuan3108@users.noreply.github.com>
2024-11-07 14:58:46 +0700

    Add files via upload

---
+++ Goods.cpp
@@ -0,0 +1,52 @@
+﻿#include "Goods.h"
+#include <iomanip>
+using namespace std;
+
+Goods::Goods() {}
+Goods::Goods(int ma, string ten, string muc, int gia, int soluong) :maHangHoa(ma), tenHangHoa(ten), danhMuc(muc), giaBan(gia), soLuongTonKho(soluong) {}
+
+	int Goods::getMaHangHoa() const { return maHangHoa; }
+	string Goods::getTenHangHoa() const { return tenHangHoa; }
+	string Goods::getDanhMuc() const { return danhMuc; }
+	int Goods::getSoLuongTonKho() const { return soLuongTonKho; }
+	int Goods::getGiaBan() const { return giaBan;  }
+	
+
+	void Goods::setThongTin(int ma, string ten, string muc, int gia, int soluong) {
+		maHangHoa = ma;
+		tenHangHoa = ten;
+		danhMuc = muc;
+		giaBan = gia;
+		soLuongTonKho = soluong;
+	}
+
+
+	void Goods::setsoLuongTonKho(int sl) {
+		soLuongTonKho = sl;
+	}
+
+	bool operator<(const Goods& g1, const Goods& g2) {
+		return g1.getGiaBan() < g2.getGiaBan();
+	}
+
+	istream& operator>>(istream& is, Goods& goods) {
+		is >> goods.maHangHoa;
+		is.ignore();
+		getline(is, goods.tenHangHoa);
+		getline(is, goods.danhMuc);
+		is >> goods.giaBan;
+		is >> goods.soLuongTonKho;
+		return is;
+	}
+
+
+	ostream& operator<<(ostream& os, const Goods& goods) {
+		os  << left 
+			<< setw(15) << goods.maHangHoa
+			<< setw(20) << goods.tenHangHoa
+			<< setw(20) << goods.danhMuc
+			<< setw(15) << goods.giaBan
+			<< setw(20) << goods.soLuongTonKho;
+		return os;
+	}
+	
---
+++ PhanA.cpp
@@ -0,0 +1,11 @@
+﻿#include "Goods.h"
+#include "ListOfGoods.h"
+#include "App.h"
+using namespace std;
+
+int main() {
+	App app;
+	app.run();
+	return 0;
+}
+
